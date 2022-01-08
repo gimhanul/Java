690a75ede7452feda2c87be8b69dec4dd148deee
gimhanul <gimhanuly@gmail.com>
2022-01-08 20:09:20 +0900

    Constructor

---
+++ src/classs/Student.java
@@ -0,0 +1,13 @@
+package classs;
+
+public class Student {
+    int num;
+    int dormitoryNumber;
+    String name;
+
+    public Student(int num, int dormitoryNumber, String name) {
+        this.num = num;
+        this.dormitoryNumber = dormitoryNumber;
+        this.name = name;
+    } // ALT + insert 단축키
+}
---
+++ src/classs/StudentTest.java
@@ -0,0 +1,8 @@
+package classs;
+
+public class StudentTest {
+    Student s1 = new Student(1206, 209, "김한울");
+    Student s2 = new Student(2210, 513, "신중빈");
+
+    //System.out.println(s1.num + s1.dormitoryNumber + s1.name)
+}

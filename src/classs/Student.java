3e2a26e340168b2236bd303291250720ae3dbb4a
gimhanul <gimhanuly@gmail.com>
2022-01-09 22:48:11 +0900

    this()

--- src/classs/Student.java
+++ src/classs/Student.java
@@ -4,16 +4,21 @@ public class Student {
     int num;
     int dormitoryNumber;
     String name;
+    String school;
 
-    public Student(int num, int dormitoryNumber, String name) {
+    public Student(String school, int num, int dormitoryNumber) {
         this.num = num;
         this.dormitoryNumber = dormitoryNumber;
+        this.school = school;
+    }
+
+    public Student(int num, int dormitoryNumber, String name) {
+        this("부산소마고", num, dormitoryNumber);
         this.name = name;
     } // ALT + insert 단축키
 
     public Student(int num, int dormitoryNumber) {
-        this.num = num;
-        this.dormitoryNumber = dormitoryNumber;
+        this("대구소마고", num, dormitoryNumber);
         this.name = "모름";
     }
 }
--- src/classs/StudentTest.java
+++ src/classs/StudentTest.java
@@ -4,9 +4,8 @@ public class StudentTest {
     public static void main(String[] args) {
         Student s1 = new Student(1206, 209, "김한울");
         Student s2 = new Student(2210, 513);
-
+        Student s3 = new Student(1111, 111, "부산소마고");
         System.out.println(s1.name);
-        System.out.print(s2.name);
-    }
-
+        System.out.print(s2.school);
+    } //main 치면 자동완성됨
 }

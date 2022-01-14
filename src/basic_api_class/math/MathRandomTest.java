6d134e60be91aee533907a1c1032dc10f5c4eaa9
gimhanul <gimhanuly@gmail.com>
2022-01-14 19:08:57 +0900

    Math random

---
+++ src/basic_api_class/math/MathRandomTest.java
@@ -0,0 +1,8 @@
+package basic_api_class.math;
+
+public class MathRandomTest {
+    public static void main(String[] args) {
+        int noon = (int) (Math.random() * 6) + 1;
+        System.out.println(noon);
+    }
+}

35ad493588bc30d582a43726057dfb34b12c2d54
gimhanul <gimhanuly@gmail.com>
2022-01-14 20:13:22 +0900

    java.util

---
+++ src/basic_api_class/util/CalendarTest.java
@@ -0,0 +1,60 @@
+package basic_api_class.util;
+
+import java.util.Calendar;
+
+public class CalendarTest {
+    public static void main(String[] args) {
+        Calendar now = Calendar.getInstance();
+
+        int year = now.get(Calendar.YEAR); //년
+        int month = now.get(Calendar.MONTH) + 1; //월
+        int day = now.get(Calendar.DAY_OF_MONTH); //일
+
+        int week = now.get(Calendar.DAY_OF_WEEK); //요일
+        String strWeek = null;
+        switch (week) {
+            case Calendar.MONDAY:
+                strWeek = "월";
+                break;
+            case Calendar.TUESDAY:
+                strWeek = "화";
+                break;
+            case Calendar.WEDNESDAY:
+                strWeek = "수";
+                break;
+            case Calendar.THURSDAY:
+                strWeek = "목";
+                break;
+            case Calendar.FRIDAY:
+                strWeek = "금";
+                break;
+            case Calendar.SATURDAY:
+                strWeek = "토";
+                break;
+            default:
+                strWeek = "일";
+        }
+
+        int amPm = now.get(Calendar.AM_PM); //오전 오후
+        String strAmPm = null;
+        if(amPm == Calendar.AM) {
+            strAmPm = "오전";
+        } else {
+            strAmPm = "오후";
+        }
+
+        int hour = now.get(Calendar.HOUR); //시
+        int minute = now.get(Calendar.MINUTE); //분
+        int second = now.get(Calendar.SECOND); //초
+
+        System.out.print(year + "년 ");
+        System.out.print(month + "월 ");
+        System.out.print(day + "일 ");
+        System.out.println(strWeek + "요일");
+
+        System.out.print(strAmPm + " ");
+        System.out.print(hour + "시 ");
+        System.out.print(minute + "분 ");
+        System.out.println(second + "초");
+    }
+}

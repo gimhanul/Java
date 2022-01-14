7a54b2d7141e56d0476814e38393d7460102a4d2
gimhanul <gimhanuly@gmail.com>
2022-01-14 12:42:41 +0900

    Exception

---
+++ src/exception/NullPointerExceptionTest.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class NullPointerExceptionTest {
+    public static void main(String[] args) {
+        String data = null;
+        System.out.println(data.toString());
+    }
+}

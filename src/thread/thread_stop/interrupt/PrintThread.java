f14ce99c7243502dd5263841f58257c127ea53ba
gimhanul <gimhanuly@gmail.com>
2022-01-15 07:06:32 +0900

    Thread stop interrupted

--- src/thread/thread_stop/interrupt/PrintThread.java
+++ src/thread/thread_stop/interrupt/PrintThread.java
@@ -3,12 +3,12 @@ package thread.thread_stop.interrupt;
 public class PrintThread extends Thread {
     @Override
     public void run() {
-        try {
-            while(true) {
-                System.out.println("실행잉");
-                Thread.sleep(1);
+        while(true) {
+            System.out.println("실행잉");
+            if(Thread.interrupted()) {
+                break;
             }
-        } catch(InterruptedException e) {}
+        }
 
         System.out.println("자원정링잉");
         System.out.println("어실행긑낫어");
